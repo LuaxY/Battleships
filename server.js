2db633210ac9dd585f9caab2e959dd275ea196d1
LuaxY <yann.guineau@voidmx.net>
2015-03-19 11:14:02 +0100

    Add index page

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http_port = 81;
+var http_port = 4242;
 
 var express = require('express');
 var app  = express();
@@ -7,6 +7,15 @@ var io   = require('socket.io')(http);
 var fs   = require('fs');
 
 app.use('/static', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    fs.readFile('html/index.html', function(err, data) {
+        console.log('[http] game page requested');
+        res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': data.length});
+        res.write(data);
+        res.end();
+    });
+});
 
 app.get('/game', function(req, res) {
     fs.readFile('html/game.html', function(err, data) {
